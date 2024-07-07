bcd1fce4dd2402b312005c3504d7369153972e82
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-07-07 09:57:35 -0400

    Fronend changes and add client credentials

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,6 @@ import './App.css'
 import PhotoBooth from './PhotoBooth'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <PhotoBooth />
@@ -14,4 +12,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
